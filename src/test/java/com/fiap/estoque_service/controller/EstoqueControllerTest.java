2768ffcc036b1288390bf6085757994f67d60865
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2025-08-03 12:17:40 -0300

    criando testes unitarios

---
+++ src/test/java/com/fiap/estoque_service/controller/EstoqueControllerTest.java
@@ -0,0 +1,2 @@
+package com.fiap.estoque_service.controller;public class EstoqueControllerTest {
+}
---
+++ src/test/java/com/fiap/estoque_service/gateway/databasejpa/EstoqueJpaGatewayTest.java
@@ -0,0 +1,4 @@
+package com.fiap.estoque_service.gateway.databasejpa;
+
+public class EstoqueJpaGatewayTest {
+}
